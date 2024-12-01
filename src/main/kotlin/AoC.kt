a59feb9536d39d49074dccfdce5a4122af113f8d
Matt Jewell <m@mattjewell.co.uk>
2024-12-01 22:51:23 +0100

    reforktererng

--- src/main/kotlin/AoC.kt
+++ src/main/kotlin/AoC.kt
@@ -1,12 +1,13 @@
 import io.github.cdimascio.dotenv.dotenv
 import java.io.File
+import java.time.LocalDate
 
 val dotenv = dotenv()
 
 fun main(args: Array<String>) {
-    val day = (args.getOrNull(0)?.toInt() ?: 1).toString().padStart(2, '0')
-    val inputType = args.getOrNull(1) ?: "input"
-    val year = 2024
+    val day = getDay(args)
+    val inputType = getInputType(args)
+    val year = getYear()
 
     val input = getInput(year, day, inputType)
     val puzzleClass = Class.forName("year_$year.Day$day").asSubclass(Puzzle::class.java)
@@ -16,7 +17,17 @@ fun main(args: Array<String>) {
     println("Part Two: " + instance.partTwo())
 }
 
+private fun getYear() = dotenv.get("YEAR")?.toInt() ?: 2024
 
+private fun getInputType(args: Array<String>) = args.getOrNull(1) ?: "input"
+
+private fun getDay(args: Array<String>) = (
+        args.getOrNull(0)?.toInt()
+            ?: dotenv.get("DAY")?.toInt()
+            ?: currentDay()
+        ).toString().padStart(2, '0')
+
+fun currentDay() = LocalDate.now().dayOfMonth.coerceAtMost(25)
 
 fun getInput(year: Int, day: String, inputType: String): String {
     val inputFileLocation = "input/$year/day_${day}_$inputType.txt"
--- src/main/kotlin/year_2024/Day01.kt
+++ src/main/kotlin/year_2024/Day01.kt
@@ -1,35 +1,27 @@
 package year_2024
 
 import Puzzle
+import kotlin.math.abs
 
 class Day01(rawInput: String) : Puzzle(rawInput) {
     override fun partOne(): String {
-        val (first, second) = toIntLists(lines())
-        var diff = 0
-        for (i in first.indices) {
-            diff += Math.abs(first[i] - second[i])
-        }
+        val (first, second) = lines().toIntLists()
+        val diff = first.indices.sumOf { i -> abs(first[i] - second[i]) }
         return diff.toString()
     }
 
     override fun partTwo(): String {
-        val (first, second) = toIntLists(lines())
+        val (first, second) = lines().toIntLists()
         val secondFreq = second.groupingBy { it }.eachCount()
-        var score = 0
-        for (i in first) {
-            score += i * (secondFreq[i] ?: 0)
-        }
+        val score = first.sumOf { firstInt -> firstInt * (secondFreq[firstInt] ?: 0) }
         return score.toString()
     }
 
-    fun toIntLists(lines: List<String>): Pair<List<Int>, List<Int>> {
-        val first = mutableListOf<Int>()
-        val second = mutableListOf<Int>()
-        lines.forEach { line ->
-            val (firstStr, secondStr) = line.split("\\s+".toRegex())
-            first.add(firstStr.toInt())
-            second.add(secondStr.toInt())
-        }
-        return first.sorted() to second.sorted()
-    }
+    private fun List<String>.toIntLists(): Pair<List<Int>, List<Int>> =
+        fold(mutableListOf<Int>() to mutableListOf<Int>()) { (firstList, secondList), line ->
+            val (first, second) = line.split("\\s+".toRegex()).map { it.toInt() }
+            firstList.add(first)
+            secondList.add(second)
+            firstList to secondList
+        }.let { it.first.sorted() to it.second.sorted() }
 }
