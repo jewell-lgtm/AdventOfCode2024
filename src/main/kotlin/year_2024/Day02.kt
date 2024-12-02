c68a8523ec5f62723e48865eb137868bba8c6f3e
Matt Jewell <m@mattjewell.co.uk>
2024-12-02 16:35:02 +0100

    2024-day02 refergderring

--- src/main/kotlin/year_2024/Day02.kt
+++ src/main/kotlin/year_2024/Day02.kt
@@ -18,9 +18,9 @@ class Day02(rawInput: String) : Puzzle(rawInput) {
     private fun <E> List<E>.permutations() =
         indices.asSequence().map { index -> until(index) + after(index) }
 
-    private fun <E> List<E>.after(index: Int) = slice(index + 1 until size)
+    private fun <E> List<E>.after(index: Int) = subList(index + 1, size)
 
-    private fun <E> List<E>.until(index: Int) = slice(0 until index)
+    private fun <E> List<E>.until(index: Int) = subList(0, index)
 
     private fun List<Int>.isSafe(): Boolean = diffs()
         .all { it in 1..3 } || diffs().all { it in -3..-1 }
