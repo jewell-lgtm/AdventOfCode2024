bfedd18976faa71a2cdd34fd291378fe7ff36ad6
Matt Jewell <m@mattjewell.co.uk>
2024-12-02 17:39:50 +0100

    2024-day01 refactor

--- src/main/kotlin/year_2024/Day01.kt
+++ src/main/kotlin/year_2024/Day01.kt
@@ -5,35 +5,32 @@ import kotlin.math.abs
 
 class Day01(rawInput: String) : Puzzle(rawInput) {
     override fun partOne(): String {
-        return lines()
-            .toIntListPair()
-            .sorted()
-            .zipped()
-            .sumOf { (a, b) -> abs(a - b) }
-            .toString()
+        val pairs = cols().sorted().zipped()
+        return pairs.pairwiseSum().toString()
     }
 
 
     override fun partTwo(): String {
-        val (numbers, counts) = lines().toIntListPair().run { first to second.toCounts() }
+        val (numbers, counts) = cols().run { first to second.toCounts() }
         return numbers.sumOf { counts.similarityScore(it) }.toString()
     }
+
+    private fun cols() = lines().map { it.toIntPair() }.unzip()
 
     private fun Pair<List<Int>, List<Int>>.sorted() =
         first.sorted() to second.sorted()
 
     private fun Pair<List<Int>, List<Int>>.zipped() = first.zip(second)
 
+    private fun List<Pair<Int, Int>>.pairwiseSum() = sumOf { (a, b) -> abs(a - b) }
+
     private fun List<Int>.toCounts() = groupingBy { it }.eachCount()
 
     private fun Map<Int, Int>.similarityScore(value: Int) = value * getOrDefault(value, 0)
 
-    private fun List<String>.toIntListPair() = map { it.toIntPair() }.unzip()
+    private fun String.toIntPair() = toInts().run { first() to last() }
 
-    private fun String.toIntPair() =
-        splitOnWhitespace()
-            .map { it.toInt() }
-            .let { it[0] to it[1] }
+    private fun String.toInts() = splitOnWhitespace().map { it.toInt() }
 
     private fun String.splitOnWhitespace() = split("""\s+""".toRegex())
 
