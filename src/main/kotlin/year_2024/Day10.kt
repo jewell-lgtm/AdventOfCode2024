9637d5c94987104e743245077a6fb2a1641a6637
Matt Jewell <m@mattjewell.co.uk>
2024-12-10 06:02:34 +0100

    idk whut

---
+++ src/main/kotlin/year_2024/Day10.kt
@@ -0,0 +1,13 @@
+package year_2024
+
+import Puzzle
+
+class Day10(rawInput: String) : Puzzle(rawInput) {
+    override fun partOne(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun partTwo(): String {
+        TODO("Not yet implemented")
+    }
+}
